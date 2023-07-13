bb272c5838723608e94c4b59fe530bb9d88053ea
Diogo Tunin <diogo.tunin@gmail.com>
2023-07-12 22:02:45 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar tag image
+  const img = document.querySelector("#profile img")
+
+ // substituir a imagem 
+ if (html.classList.contains('light')){
+ // se tiver light mode, imagem light
+  img.setAttribute('src', './assets/avatar-light.png')
+  img.setAttribute('alt', 'Foto de Myke LIGHT')
+  } else {
+ // se tiver sem light, imagem normal
+  img.setAttribute('src', './assets/avatar.png')
+  img.setAttribute('alt', 'Foto de Myke')
+  }
+
+
+
+  
+  // substituir imagem
+}
