22f44735ad370a86dfc68362f6a62ad048817d41
Diogo Tunin <diogo.tunin@gmail.com>
2023-07-13 21:10:20 -0300

    model Diogo

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function toggleMode() {
  // substituir a imagem 
  if (html.classList.contains('light')){
  // se tiver light mode, imagem light
-  img.setAttribute('src', './assets/avatar-light.png')
+  img.setAttribute('src', './assets/diogo-light.png')
   img.setAttribute('alt', 'Foto de Myke LIGHT')
   } else {
  // se tiver sem light, imagem normal
-  img.setAttribute('src', './assets/avatar.png')
+  img.setAttribute('src', './assets/diogo.png')
   img.setAttribute('alt', 'Foto de Myke')
   }
 
